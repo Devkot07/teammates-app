28412a0432b6b0823ddc4af56e1ae8529c646d9a
Devkot07 <devkot07@gmail.com>
2025-02-15 01:20:56 +0300

    Rewrite Toasts logic

--- app/src/main/java/com/pezont/teammates/ui/screens/TemmatesApp.kt
+++ app/src/main/java/com/pezont/teammates/ui/screens/TemmatesApp.kt
@@ -1,23 +1,18 @@
 package com.pezont.teammates.ui.screens
 
-import android.content.Context
-import android.widget.Toast
+
 import androidx.compose.material3.Text
-
-
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-
 import com.pezont.teammates.R
 import com.pezont.teammates.models.ContentType
 import com.pezont.teammates.ui.TeammatesUiState
 import com.pezont.teammates.ui.TeammatesViewModel
-import com.pezont.teammates.ui.navigation.ProfileNavHost
-import com.pezont.teammates.ui.screens.sendToasts.SendNetworkErrorToast
+import com.pezont.teammates.ui.screens.sendToasts.sendLoginToast
 
 @Composable
 fun TeammatesApp(
@@ -25,18 +20,28 @@ fun TeammatesApp(
     viewModel: TeammatesViewModel,
 
 ){
+
     val context = LocalContext.current
+
+    LaunchedEffect(Unit) {
+        viewModel.loginToastCode.collect { code ->
+            sendLoginToast(code,context)
+            //Toast.makeText(context, "$code", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
 
     when (val teammatesUiState = viewModel.teammatesUiState.collectAsState().value) {
         is TeammatesUiState.Loading -> TeammatesLoadingScreen()
         is TeammatesUiState.Login -> {
-            SendNetworkErrorToast(teammatesUiState.statusResponse, context)
+
+            //SendLoginErrorToast(code, context)
             LoginScreen(viewModel, teammatesUiState)
         }
 
         is TeammatesUiState.Home -> {
             TeammatesHomeScreen(
-                currentItem = teammatesUiState.currentItem,
                 onTabPressed = { contentType: ContentType ->
                     viewModel.updateCurrentContent(contentType)
                 },
--- app/src/main/java/com/pezont/teammates/ui/screens/sendToasts/SendToasts.kt
+++ app/src/main/java/com/pezont/teammates/ui/screens/sendToasts/SendToasts.kt
@@ -6,17 +6,38 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import com.pezont.teammates.R
 
-// TODO: make general
-@Composable
-fun SendNetworkErrorToast(status: Int, context: Context) {
+
+fun sendLoginToast(status: Int?, context: Context) {
     when (status){
         400 -> {
             Toast.makeText(context,
-                stringResource(R.string.authorization_error_incorrect_login_or_password), Toast.LENGTH_SHORT).show()}
-        401 ->{ Toast.makeText(context,
-            stringResource(R.string.authorization_error_authorization_failed), Toast.LENGTH_SHORT).show()}
-        1 -> { Toast.makeText(context, stringResource(R.string.logout), Toast.LENGTH_SHORT).show()}
+                R.string.authorization_error_incorrect_login_or_password,
+                Toast.LENGTH_SHORT).show()
+        }
+        401 ->{
+            Toast.makeText(context,
+            R.string.authorization_error_authorization_failed,
+                Toast.LENGTH_SHORT).show()
+        }
+        1 -> { Toast.makeText(context,
+            R.string.logout,
+            Toast.LENGTH_SHORT).show()}
         else -> {}
     }
-
 }
+
+@Composable
+fun SendGetQuestionnairesToast(status: Int, context: Context) {
+    when (status){
+//        400 -> { Toast.makeText(context,
+//            stringResource(),
+//                Toast.LENGTH_SHORT).show()}
+        401 ->{ Toast.makeText(context,
+            stringResource(R.string.authorization_error_authorization_failed), Toast.LENGTH_SHORT).show()}
+        1 -> { Toast.makeText(context,
+            stringResource(R.string.logout), Toast.LENGTH_SHORT).show()}
+        else -> {}
+    }
+}
+
+
