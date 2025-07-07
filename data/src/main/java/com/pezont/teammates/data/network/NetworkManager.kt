dfbbe23fa87fd78d44de8686e65604618cc885f7
Devkot07 <devkot07@gmail.com>
2025-07-07 23:50:48 +0000

    Create NetworkManager

---
+++ data/src/main/java/com/pezont/teammates/data/network/NetworkManager.kt
@@ -0,0 +1,16 @@
+package com.pezont.teammates.data.network
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+object NetworkManager {
+    @SuppressLint("ServiceCast")
+    fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork ?: return false
+        val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
+        return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+    }
+}
